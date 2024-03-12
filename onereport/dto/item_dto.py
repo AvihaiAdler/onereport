4a285d55ec30b0b8b60a1cae89813672abec7648
adler <avihai.adler@gmail.com>
2024-03-12 17:32:02 +0200

    - removed `controller.py`. - removed `item_dto.py`

--- onereport/dto/item_dto.py
+++
@@ -1,9 +0,0 @@
-from typing import Self
-
-class ItemDTO():
-  def __init__(self: Self, type: str, space: str, created_by: str, attributes: dict[str, str] = {}, /) -> None:
-    self.type = type.lower()
-    self.space = space.lower()
-    self.created_by = created_by
-    self.attributes = attributes
-    
