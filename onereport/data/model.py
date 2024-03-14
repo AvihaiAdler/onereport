10ab8edef194fba11c7687b1c9482efeabbfbe96
adler <avihai.adler@gmail.com>
2024-03-14 14:30:22 +0200

    - simplified all data entities.

--- onereport/data/model.py
+++ onereport/data/model.py
@@ -1,15 +1,50 @@
-import flask_sqlalchemy
+from typing import Self, Set
 import sqlalchemy.orm as orm
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 import sqlalchemy
 
-class Base(orm.DeclarativeBase, orm.MappedAsDataclass):
+class Base(orm.DeclarativeBase):
   pass
 
-db = flask_sqlalchemy.SQLAlchemy(model_class=Base)
+db = SQLAlchemy(model_class=Base)
 
-personal_date_table = sqlalchemy.Table(
+class User(db.Model):
+  email: orm.Mapped[str] = orm.mapped_column(primary_key=True)
+  username: orm.Mapped[str]
+  role: orm.Mapped[str]
+  company: orm.Mapped[str]
+  active: orm.Mapped[bool]= orm.mapped_column(default=True)
+  
+  def __repr__(self: Self) -> str:
+    return f"User(email: {self.email}, username: {self.username}, role: {self.role}, company: {self.company}, active: {self.active})"
+
+personnel_date = sqlalchemy.Table(
   "personnel_date",
   Base.metadata,
   sqlalchemy.Column("date_id", sqlalchemy.ForeignKey("date.id"), primary_key=True),
   sqlalchemy.Column("personnel_id", sqlalchemy.ForeignKey("personnel.id"), primary_key=True),
 )
+
+class Personnel(db.Model):
+  id: orm.Mapped[str] = orm.mapped_column(primary_key=True)
+  first_name: orm.Mapped[str]
+  last_name: orm.Mapped[str]
+  company: orm.Mapped[str]
+  platoon: orm.Mapped[str]
+  active: orm.Mapped[bool] = orm.mapped_column(default=True)
+  
+  dates_present: orm.Mapped[Set["Date"]] = orm.relationship(secondary=personnel_date, back_populates="present", lazy=True)
+  
+  def __repr__(self: Self) -> str:
+    return f"Personnel(id: {self.id}, full name:{' '.join((self.first_name, self.last_name))}, company: {self.company}, platoon: {self.platoon}, active: {self.active})"
+  
+
+class Date(db.Model):
+  id: orm.Mapped[datetime.date] = orm.mapped_column(primary_key=True, default=datetime.date.today)
+  
+  present: orm.Mapped[Set["Personnel"]] = orm.relationship(secondary=personnel_date, back_populates="dates_present", lazy=True)
+
+  def __repr__(self: Self) -> str:
+    return f"Date({self.id.day}/{self.id.month}/{self.id.year})"
+
--- onereport/data/user_entity.py
+++
@@ -1,9 +0,0 @@
-import model
-import sqlalchemy.orm as orm
-
-class User(model.db.Model):
-  email: orm.Mapped[str] = orm.mapped_column(primary_key=True)
-  username: orm.Mapped[str]
-  role: orm.Mapped[str]
-  space: orm.Mapped[str]
-  active: orm.Mapped[bool]= orm.mapped_column(default=True)
