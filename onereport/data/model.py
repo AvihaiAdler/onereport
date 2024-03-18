a399fb77bd238276156ae7388d9545c2b29a1f7f
adler <avihai.adler@gmail.com>
2024-03-18 15:08:47 +0200

    data changes: - added `flask_login`. Users can now be managed by it. - added `__init__` methods for all model classed since using keyword args methods pisses me off. - added the necessary functionality to `User` in order for the login manager to recognize `User::email` as its `id`. - added the necessary functionality to `Date` in order for the login manager to recognize `Date::id` as its `id`.

--- onereport/data/model.py
+++ onereport/data/model.py
@@ -3,21 +3,34 @@ import sqlalchemy.orm as orm
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import sqlalchemy
+from flask_login import UserMixin, LoginManager
 
 class Base(orm.DeclarativeBase):
   pass
 
 db = SQLAlchemy(model_class=Base)
 
-class User(db.Model):
+login_manager = LoginManager()
+
+class User(db.Model, UserMixin):
   email: orm.Mapped[str] = orm.mapped_column(primary_key=True)
   username: orm.Mapped[str]
   role: orm.Mapped[str]
   company: orm.Mapped[str]
   active: orm.Mapped[bool]= orm.mapped_column(default=True)
   
+  def __init__(self, email: str, username: str, role: str, company: str, /) -> None:
+    super().__init__(email=email, username=username, role=role, company=company)
+  
   def __repr__(self: Self) -> str:
     return f"User(email: {self.email}, username: {self.username}, role: {self.role}, company: {self.company}, active: {self.active})"
+  
+  def get_id(self: Self) -> str:
+    return self.email
+
+@login_manager.user_loader
+def load_user(email: str) -> User:
+  return db.session.get(User, email)
 
 personnel_date = sqlalchemy.Table(
   "personnel_date",
@@ -36,6 +49,9 @@ class Personnel(db.Model):
   
   dates_present: orm.Mapped[Set["Date"]] = orm.relationship(secondary=personnel_date, back_populates="present", lazy=True)
   
+  def __init__(self, id: str, first_name: str, last_name: str, company: str, platoon: str, /) -> None:
+    super().__init__(id=id, first_name=first_name, last_name=last_name, company=company, platoon=platoon)
+  
   def __repr__(self: Self) -> str:
     return f"Personnel(id: {self.id}, full name:{' '.join((self.first_name, self.last_name))}, company: {self.company}, platoon: {self.platoon}, active: {self.active})"
   
@@ -44,6 +60,12 @@ class Date(db.Model):
   id: orm.Mapped[datetime.date] = orm.mapped_column(primary_key=True, default=datetime.date.today)
   
   present: orm.Mapped[Set["Personnel"]] = orm.relationship(secondary=personnel_date, back_populates="dates_present", lazy=True)
+
+  def __init__(self) -> None:
+    super().__init__()
+    
+  def get_id(self: Self) -> datetime.date:
+    return Self.id
 
   def __repr__(self: Self) -> str:
     return f"Date({self.id.day}/{self.id.month}/{self.id.year})"
