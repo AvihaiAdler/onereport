2eb156125da7cdb355b4a6394ce737c26f6c560b
adler <avihai.adler@gmail.com>
2024-03-20 13:23:43 +0200

    - changed the old `order` module to `order_attr` for accesses of `Order` and `OrderBy`

--- onereport/dal/user_dal.py
+++ onereport/dal/user_dal.py
@@ -10,7 +10,7 @@ def get_user_by_email(email: str, /) -> model.User | None:
   return model.db.session.scalar(sqlalchemy.select(model.User).filter(model.User.email == email))
 
 def construct_statement(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> sqlalchemy.Select[tuple]:
-  return sqlalchemy.select(model.User).order_by(sqlalchemy.asc(order_by.value)) if order == order.Order.ASC else sqlalchemy.select(model.User).order_by(sqlalchemy.desc(order_by.value))
+  return sqlalchemy.select(model.User).order_by(sqlalchemy.asc(order_by.value)) if order == order_attr.Order.ASC else sqlalchemy.select(model.User).order_by(sqlalchemy.desc(order_by.value))
 
 def get_all_active_users(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> list[model.User]:
   statement = construct_statement(order_by, order)
