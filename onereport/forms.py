3d7203fe1d35c9770f8d84dcc179d683591827d2
adler <avihai.adler@gmail.com>
2024-03-20 16:21:13 +0200

    - changed `UserRegistrationForm` to accommodate the changes in `User`. - replaced old sqlalchemy syntax with our DAL methods.

--- onereport/forms.py
+++ onereport/forms.py
@@ -1,32 +1,33 @@
 import flask_wtf
 import wtforms
 import wtforms.validators as validators
-from .data.misc import Company, Role
-from .data import model
+from onereport.data import misc
+from onereport.dal import personnel_dal, user_dal
 
 class PersonnelRegistrationFrom(flask_wtf.FlaskForm):
-  id = wtforms.StringField("מספר אישי", validators=[validators.DataRequired(), validators.Length(7,7)])
-  first_name = wtforms.StringField("שם פרטי", validators=[validators.DataRequired(), validators.Length(2, 10)])
-  last_name = wtforms.StringField("שם משפחה", validators=[validators.DataRequired(), validators.Length(2, 15)])
-  company = wtforms.SelectField("פלוגה", choices=[(name, member.value) for name, member in Company.__members__.items()])
+  id = wtforms.StringField("מספר אישי", validators=[validators.InputRequired("שדה חובה"), validators.Length(7,7)])
+  first_name = wtforms.StringField("שם פרטי", validators=[validators.InputRequired("שדה חובה"), validators.Length(2, 10)])
+  last_name = wtforms.StringField("שם משפחה", validators=[validators.InputRequired("שדה חובה"), validators.Length(2, 15)])
+  company = wtforms.SelectField("פלוגה", choices=[(name, member.value) for name, member in misc.Company.__members__.items()])
   
   submit = wtforms.SubmitField("הוסף חייל")
   
   def validate_id(self, id: wtforms.StringField) -> None:
-    personnel = model.Personnel.query.filter_by(id=id.data).first()
+    personnel = personnel_dal.get_personnel_by_id(id.data)
     if personnel:
       raise wtforms.ValidationError("Personnel already exists")
   
   
 class UserRegistrationFrom(flask_wtf.FlaskForm):
-  email = wtforms.EmailField("אימייל", validators=[validators.DataRequired(), validators.Email()])
-  username = wtforms.StringField("שם", validators=[validators.DataRequired(), validators.Length(2, 25)])
-  company = wtforms.SelectField("פלוגה", choices=[(name, member.value) for name, member in Company.__members__.items()])
-  role = wtforms.SelectField("תפקיד", choices=[(name, member.value) for name, member in Role.__members__.items()])
-  
+  email = wtforms.EmailField("אימייל", validators=[validators.InputRequired("שדה חובה"), validators.Email("שדה לא תקין")])
+  first_name = wtforms.StringField("שם פרטי", validators=[validators.InputRequired("שדה חובה"), validators.Length(2, 25)])
+  last_name = wtforms.StringField("שם משפחה", validators=[validators.InputRequired("שדה חובה"), validators.Length(2, 25)])
+  company = wtforms.SelectField("פלוגה", choices=[(name, member.value) for name, member in misc.Company.__members__.items()])
+  role = wtforms.SelectField("תפקיד", choices=[(name, member.value) for name, member in misc.Role.__members__.items()])
+
   submit = wtforms.SubmitField("הוסף משתמש")
   
   def validate_email(self, email: wtforms.StringField) -> None:
-    user = model.User.query.filter_by(email=email.data).first()
+    user = user_dal.get_user_by_email(email.data)
     if user:
       raise wtforms.ValidationError("User already exists")
