7394009ea7233ffabfbc9528674e1b07e7d04589
adler <avihai.adler@gmail.com>
2024-03-22 10:35:58 +0200

    - changed all `get_*` functions to `find_*`.

--- onereport/dal/personnel_dal.py
+++ onereport/dal/personnel_dal.py
@@ -3,23 +3,23 @@ from onereport.data import model
 from onereport.data import misc
 import sqlalchemy
 
-def get_personnel_by_id(id: str) -> model.Personnel | None:
+def find_personnel_by_id(id: str) -> model.Personnel | None:
   return model.db.session.scalar(sqlalchemy.select(model.Personnel).filter(model.Personnel.id == id))
 
-def get_personnel_by_first_name(first_name: str) -> model.Personnel | None:
+def find_personnel_by_first_name(first_name: str) -> model.Personnel | None:
   return model.db.session.scalar(sqlalchemy.select(model.Personnel).filter(model.Personnel.first_name == first_name))
 
-def get_personnel_by_last_name(last_name: str) -> model.Personnel | None:
+def find_personnel_by_last_name(last_name: str) -> model.Personnel | None:
   return model.db.session.scalar(sqlalchemy.select(model.Personnel).filter(model.Personnel.last_name == last_name))
 
 def construct_statement(order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> sqlalchemy.Select[tuple]:
   return sqlalchemy.select(model.Personnel).order_by(sqlalchemy.asc(order_by.name.lower())) if order == order_attr.Order.ASC else sqlalchemy.select(model.Personnel).order_by(sqlalchemy.desc(order_by.name.lower()))
 
-def get_all_active_personnel(order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> list[model.Personnel]:
+def find_all_active_personnel(order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> list[model.Personnel]:
   return model.db.session.scalars(construct_statement(order_by, order).filter(model.Personnel.active)).all()
 
-def get_all_active_personnel_by_company(company: misc.Company, order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> list[model.Personnel]:
+def find_all_active_personnel_by_company(company: misc.Company, order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> list[model.Personnel]:
   return model.db.session.scalars(construct_statement(order_by, order).filter(model.Personnel.active).filter(model.Personnel.company == company.name)).all()
 
-def get_all_personnel(order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> list[model.Personnel]:
+def find_all_personnel(order_by: order_attr.PersonnelOrderBy, order: order_attr.Order, /) -> list[model.Personnel]:
   return model.db.session.scalars(construct_statement(order_by, order)).all()
--- onereport/dal/user_dal.py
+++ onereport/dal/user_dal.py
@@ -3,24 +3,24 @@ from onereport.data import misc
 from onereport.dal import order_attr
 import sqlalchemy
 
-def get_users_by_first_name(first_name: str, /) -> model.User | None:
+def find_users_by_first_name(first_name: str, /) -> model.User | None:
   return model.db.session.scalars(sqlalchemy.select(model.User).filter(model.User.first_name == first_name)).all()
 
-def get_users_by_last_name(last_name: str, /) -> model.User | None:
+def find_users_by_last_name(last_name: str, /) -> model.User | None:
   return model.db.session.scalars(sqlalchemy.select(model.User).filter(model.User.last_name == last_name)).all()
 
-def get_user_by_email(email: str, /) -> model.User | None:
+def find_user_by_email(email: str, /) -> model.User | None:
   return model.db.session.scalar(sqlalchemy.select(model.User).filter(model.User.email == email))
 
 def construct_statement(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> sqlalchemy.Select[tuple]:
   return sqlalchemy.select(model.User).order_by(sqlalchemy.asc(order_by.name.lower())) if order == order_attr.Order.ASC else sqlalchemy.select(model.User).order_by(sqlalchemy.desc(order_by.name.lower()))
 
-def get_all_active_users(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> list[model.User]:
+def find_all_active_users(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> list[model.User]:
   statement = construct_statement(order_by, order)
   
   return model.db.session.scalars(statement.filter(model.User.active).filter(model.User.role != misc.Role.ADMIN)).all()
 
-def get_all_users(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> list[model.User]:
+def find_all_users(order_by: order_attr.UserOrderBy, order: order_attr.Order, /) -> list[model.User]:
   statement = construct_statement(order_by, order)
   
   return model.db.session.scalars(statement).all()
