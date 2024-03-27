18dcc07dea0fbef265b9022f8e816c3666343d9c
adler <avihai.adler@gmail.com>
2024-03-27 15:28:31 +0200

    - removed `find_all_report_by_date`. - added `find_report_by_id`, `find_report_by_id_and_company` & `find_all_reports_by_company`.

--- onereport/dal/report_dal.py
+++ onereport/dal/report_dal.py
@@ -1,10 +1,20 @@
-from typing import List
+from onereport.dal import order_attr
 from onereport.data import model, misc
 import sqlalchemy
 import datetime
 
+def find_report_by_id(id: int, /) -> model.Report | None:
+  return model.db.session.scalar(sqlalchemy.select(model.Report).filter(model.Report.id == id))
+
+def find_report_by_id_and_company(id: int, company: misc.Company, /) -> model.Report | None:
+  return model.db.session.scalar(sqlalchemy.select(model.Report).filter(model.Report.id == id).filter(model.Report.company == company.name))
+
 def find_report_by_date_and_company(date: datetime.date, company: misc.Company, /) -> model.Report | None:
   return model.db.session.scalar(sqlalchemy.select(model.Report).filter(model.Report.date == date).filter(model.Report.company == company.name))
 
-def find_all_report_by_date(date: datetime.date, /) -> List[model.Report]:
-  return model.db.session.scalars(sqlalchemy.select(model.Report).filter(model.Report.date == date).order_by(sqlalchemy.asc(model.Report.company))).all()
+def find_all_reports_by_company(company: misc.Company, order: order_attr.Order, /) -> list[model.Report]:
+  return model.db.session.scalars(
+    sqlalchemy.select(model.Report)
+              .filter(model.Report.company == company.name)
+              .order_by(sqlalchemy.asc(model.Report.date) if order == order_attr.Order.ASC else sqlalchemy.desc(model.Report.date))
+    ).all()
