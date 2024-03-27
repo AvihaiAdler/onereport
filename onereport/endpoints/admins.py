786c1f1478214827c7afff3366daabd871c86cb2
adler <avihai.adler@gmail.com>
2024-03-27 15:32:41 +0200

    - removed all duplicate functions and replaced them with calls to the managers API. - functions with optional query params now use them properly.

--- onereport/endpoints/admins.py
+++ onereport/endpoints/admins.py
@@ -1,5 +1,4 @@
-from onereport import app, forms
-from onereport.data import model
+from onereport import app, forms, generate_urlstr
 from onereport.data import misc
 from onereport.dto import user_dto, personnel_dto
 from onereport.dal import user_dal, personnel_dal
@@ -13,104 +12,32 @@ def not_admin() -> bool:
 @app.route("/onereport/admins/register_personnel", methods=["GET", "POST"])
 @flask_login.login_required
 def a_register_personnel() -> str:
-  if not_admin():
-    return flask.redirect(flask.url_for("home"))
-  
-  form = forms.PersonnelRegistrationFrom()
-  
-  if form.validate_on_submit():
-    personnel = model.Personnel(form.id.data.strip(), form.first_name.data.strip(), form.last_name.data.strip(), form.company.data, "1")
-    
-    # SELECT p FROM personnel WHERE p.id == personnel.id
-    old_personnel = personnel_dal.find_personnel_by_id(personnel.id)
-    if old_personnel is not None:
-      # model.db.session.delete(old_personnel)
-      old_personnel.update(personnel)
-      model.db.session.commit()
-      flask.flash(f"החייל {' '.join((old_personnel.first_name, old_personnel.last_name))} עודכן בהצלחה", category="success")
-    else:  
-      model.db.session.add(personnel)
-      model.db.session.commit()
-      flask.flash(f"החייל {' '.join((personnel.first_name, personnel.last_name))} נוסף בהצלחה", category="success")
-    
-    return flask.redirect(flask.url_for("home"))
-    
-  return flask.render_template("personnel_registration.html", form=form)
+  return flask.redirect(flask.url_for(generate_urlstr(misc.Role.MANAGER.name, "register_personnel")))
 
 @app.route("/onereport/admins/register_user", methods=["GET", "POST"])
 @flask_login.login_required
 def a_register_user() -> str:
-  if not_admin():
-    return flask.redirect(flask.url_for("home"))
-  
-  form = forms.UserRegistrationFrom()
-  
-  if form.validate_on_submit():
-    user = model.User(form.email.data.strip(), form.first_name.data.strip(), form.last_name.data.strip(), form.role.data, form.company.data)
-    
-    # SELECT old_user FROM users WHERE old_user.email == user.email
-    old_user = user_dal.find_user_by_email(user.email)
-    if old_user is not None:
-      # model.db.session.delete(old_user)
-      old_user.update(user)
-      model.db.session.commit()
-      flask.flash(f"הפקיד {' '.join((old_user.first_name, old_user.last_name))} עודכן בהצלחה", category="success")
-    else:
-      model.db.session.add(user)
-      model.db.session.commit()
-      flask.flash(f"הפקיד {' '.join((user.first_name, user.last_name))} נוסף בהצלחה", category="success")
-    
-    return flask.redirect(flask.url_for("home"))
-  
-  return flask.render_template("user_registration.html", form=form)
+  return flask.redirect(flask.url_for(generate_urlstr(misc.Role.MANAGER.name, "register_user")))
 
 @app.route("/onereport/admins/personnel/<id>/update", methods=["GET", "POST"])
 @flask_login.login_required
 def a_update_personnel(id: str) -> str:
-  if not_admin():
-    return flask.redirect(flask.url_for("home"))
-  
-  old_personnel = personnel_dal.find_personnel_by_id(id)
-  if old_personnel is None:
-    print(f"couldn't find {id}")
-    flask.flash(f"החייל {id} אינו במסד הנתונים", category="danger")
-    return flask.redirect(flask.url_for("a_get_all_personnel"))
-  
-  print("personel found")
-  form = forms.PersonnelUpdateForm()
-  if form.validate_on_submit():
-    personnel = model.Personnel(old_personnel.id, form.first_name.data.strip(), form.last_name.data.strip(), form.company.data, "1")
-    if misc.Active.is_valid(form.active.data):
-      personnel.active = misc.Active[form.active.data] == misc.Active.ACTIVE 
-    
-    old_personnel.update(personnel)
-    
-    model.db.session.commit()
-    
-    flask.flash(f"החייל {id} עודכן בהצלחה", category="success")
-    return flask.redirect(flask.url_for("a_get_all_personnel"))
-  
-  if flask.request.method == "GET":
-    form.id.data = old_personnel.id
-    form.first_name.data, form.last_name.data = old_personnel.first_name, old_personnel.last_name
-    form.company.data = old_personnel.company
-    form.active.data = old_personnel.active
-  
-    return flask.render_template("personnel.html", form=form)  
-  
-  return flask.redirect(flask.url_for("a_get_all_personnel"))
+  return flask.redirect(flask.url_for(generate_urlstr(misc.Role.MANAGER.name, "update_personnel"), id=id))
 
 # TODO:
 # pagination
 @app.route("/onereport/admins/users", methods=["GET", "POST"])
 @flask_login.login_required
-def a_get_all_users(order_by: str = "COMPANY", order: str = "ASC") -> str:
+def a_get_all_users() -> str:
+  order_by = flask.request.args.get("order_by", default="COMPANY")
+  order = flask.request.args.get("order", "ASC")
+  
   if not_admin():
     return flask.redirect(flask.url_for("home"))
   
   if not order_attr.UserOrderBy.is_valid(order_by):
     flask.flash(f"אין אפשרות לסדר את העצמים לפי {order_by}", category="info")
-    return flask.render_template("users.html", users=[])
+    return flask.render_template("users/users.html", users=[])
 
   if not order_attr.Order.is_valid(order):
     flask.flash(f"אין אפשרות לסדר את העצמים בסדר {order}", category="info")
@@ -118,22 +45,25 @@ def a_get_all_users(order_by: str = "COMPANY", order: str = "ASC") -> str:
   
   # SELECT * from users
   users = user_dal.find_all_users(order_attr.UserOrderBy[order_by], order_attr.Order[order])
-  return flask.render_template("users.html", users=[user_dto.UserDTO(user) for user in users])
+  return flask.render_template("users/users.html", users=[user_dto.UserDTO(user) for user in users])
 
 @app.route("/onereport/admins/personnel", methods=["GET", "POST"])
 @flask_login.login_required
-def a_get_all_personnel(order_by: str = "LAST_NAME", order: str = "ASC") -> str:
+def a_get_all_personnel() -> str:
+  order_by = flask.request.args.get("order_by", default="LAST_NAME")
+  order = flask.request.args.get("order", "ASC")
+  
   if not_admin():
     return flask.redirect(flask.url_for("home"))
   
   form = forms.PersonnelListForm()
   if not order_attr.PersonnelOrderBy.is_valid(order_by):
     flask.flash(f"אין אפשרות לסדר את העצמים לפי {order_by}", category="info")
-    return flask.render_template("personnel_list.html", form=form, personnel=[])
+    return flask.render_template("personnel/personnel_list.html", form=form, personnel=[])
 
   if not order_attr.Order.is_valid(order):
     flask.flash(f"אין אפשרות לסדר את העצמים בסדר {order}", category="info")
-    return flask.render_template("personnel_list.html", form=form, personnel=[])
+    return flask.render_template("personnel/personnel_list.html", form=form, personnel=[])
   
   if form.validate_on_submit():
     order_by = order_attr.PersonnelOrderBy[form.order_by.data]
@@ -145,10 +75,23 @@ def a_get_all_personnel(order_by: str = "LAST_NAME", order: str = "ASC") -> str:
   
   # SELECT * FROM personnel WHERE personnel.active ORDER_BY order_by order
   personnel = personnel_dal.find_all_personnel(order_by, order)
-  return flask.render_template("personnel_list.html", form=form, personnel=[personnel_dto.PersonnelDTO(p) for p in personnel])
+  return flask.render_template("personnel/personnel_list.html", form=form, personnel=[personnel_dto.PersonnelDTO(p) for p in personnel])
 
-@app.route("/onereport/users/report", methods=["GET", "POST"])
+@app.route("/onereport/admins/report", methods=["GET", "POST"])
+@flask_login.login_required
 def a_create_report() -> str:
-  return "nothing to see here"
+  return flask.redirect(flask.url_for(generate_urlstr(misc.Role.MANAGER.name, "create_report")))
+
+@app.get("/onereport/admins/reports")
+@flask_login.login_required
+def a_get_all_reports() -> str:
+  company = flask.request.args.get("company", default="")
+  order = flask.request.args.get("order", default="DESC")
+  return flask.redirect(flask.url_for(generate_urlstr(misc.Role.MANAGER.name, "get_all_reports"), company=company, order=order))
+
+@app.get("/onereport/admins/report/<int:id>")
+@flask_login.login_required
+def a_get_report(id: int) -> str:
+  return flask.redirect(flask.url_for(generate_urlstr(misc.Role.MANAGER.name, "get_report"), id=id))
 
  
