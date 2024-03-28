ee796370d592022dfdb171f94458b1352bbccfc9
adler <avihai.adler@gmail.com>
2024-03-28 15:21:45 +0200

    - added a `Platoon` enum

--- onereport/data/misc.py
+++ onereport/data/misc.py
@@ -6,7 +6,7 @@ class Role(Enum):
   ADMIN = "מנהל"
   
   @staticmethod
-  def is_valid(role: str) -> bool:
+  def is_valid(role: str, /) -> bool:
     return role in Role._member_names_
   
 class Company(Enum):
@@ -17,15 +17,31 @@ class Company(Enum):
   HEADQUARTERS = "מפקדה"
   
   @staticmethod
-  def is_valid(company: str) -> bool:
+  def is_valid(company: str, /) -> bool:
     return company in Company._member_names_
   
+class Platoon(Enum):
+  UNCATEGORIZED = "לא משוייך"
+  FIRST = "1"
+  SECOND = "2"
+  THIRD = "3"
+  FORTH = "4"
+  FIFTH = "5"
+  SIXTH = "6"
+  SEVENTH = "7"
+  EIGHTH = "8"
+  NINTH = "9"
+  
+  @staticmethod
+  def is_valid(platoon: str, /) -> bool:
+    return platoon in Platoon._member_names_  
+
 class Active(Enum):
   ACTIVE = "פעיל"
   INACTIVE = "לא פעיל"
   
   @staticmethod
-  def is_valid(active: str) -> bool:
+  def is_valid(active: str, /) -> bool:
     return active in Active._member_names_ 
   
 class Presence(Enum):
@@ -33,5 +49,5 @@ class Presence(Enum):
   NOT_PRESENT = "לא נמצא"
   
   @staticmethod
-  def is_valid(presence: str) -> bool:
+  def is_valid(presence: str, /) -> bool:
     return presence in Presence._member_names_
