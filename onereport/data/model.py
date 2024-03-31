0dc6f132788eb808a6cd3851bdfeceea63ef06da
adler <avihai.adler@gmail.com>
2024-03-31 16:17:26 +0300

    reformatted the model to use class hierarchy: `User` now inherite from `Personnel`

--- onereport/data/model.py
+++ onereport/data/model.py
@@ -11,52 +11,7 @@ class Base(orm.DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
-
 login_manager = LoginManager()
-
-
-class User(db.Model, UserMixin):
-    email: orm.Mapped[str] = orm.mapped_column(primary_key=True)
-    first_name: orm.Mapped[str]
-    last_name: orm.Mapped[str]
-    role: orm.Mapped[str]
-    company: orm.Mapped[str]
-    active: orm.Mapped[bool] = orm.mapped_column(default=True)
-
-    def __init__(
-        self: Self,
-        email: str,
-        first_name: str,
-        last_name: str,
-        role: str,
-        company: str,
-        /,
-    ) -> None:
-        super().__init__(
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            role=role,
-            company=company,
-        )
-
-    def update(self: Self, other: Self) -> None:
-        self.first_name, self.last_name = other.first_name, other.last_name
-        self.company = other.company
-        self.active = other.active
-        self.role = other.role
-
-    def __repr__(self: Self) -> str:
-        return f"User(email: {self.email}, name: {' '.join((self.first_name, self.last_name))}, role: {self.role}, company: {self.company}, active: {self.active})"
-
-    def get_id(self: Self) -> str:
-        return self.email
-
-
-@login_manager.user_loader
-def load_user(email: str) -> User:
-    return db.session.get(User, email)
-
 
 personnel_report_rel = sqlalchemy.Table(
     "personnel_report_rel",
@@ -71,12 +26,20 @@ personnel_report_rel = sqlalchemy.Table(
 
 
 class Personnel(db.Model):
+    __tablename__ = "personnel"
+
     id: orm.Mapped[str] = orm.mapped_column(primary_key=True)
     first_name: orm.Mapped[str]
     last_name: orm.Mapped[str]
     company: orm.Mapped[str]
     platoon: orm.Mapped[str]
     active: orm.Mapped[bool] = orm.mapped_column(default=True)
+
+    type: orm.Mapped[str]
+    __mapper_args__ = {
+        "polymorphic_identity": "personnel",
+        "polymorphic_on": "type",
+    }
 
     dates_present: orm.Mapped[Set["Report"]] = orm.relationship(
         secondary=personnel_report_rel, back_populates="presence", lazy=True
@@ -90,6 +53,7 @@ class Personnel(db.Model):
         company: str,
         platoon: str,
         /,
+        **kwargs,
     ) -> None:
         super().__init__(
             id=id,
@@ -97,6 +61,7 @@ class Personnel(db.Model):
             last_name=last_name,
             company=company,
             platoon=platoon,
+            **kwargs,
         )
 
     def update(self: Self, other: Self) -> None:
@@ -123,3 +88,51 @@ class Report(db.Model):
 
     def __repr__(self: Self) -> str:
         return f"Report(date: {self.date.day}/{self.date.month}/{self.date.year}, company: {self.company})"
+
+
+class User(Personnel, UserMixin):
+    __tablename__ = "user"
+
+    id: orm.Mapped[str] = orm.mapped_column(
+        sqlalchemy.ForeignKey("personnel.id"), primary_key=True
+    )
+    email: orm.Mapped[str]
+    role: orm.Mapped[str]
+
+    __mapper_args__ = {
+        "polymorphic_identity": "user",
+    }
+    
+    def __init__(
+        self: Self,
+        id: str,
+        email: str,
+        first_name: str,
+        last_name: str,
+        role: str,
+        company: str,
+        platoon: str,
+        /,
+    ) -> None:
+        super().__init__(
+            id, first_name, last_name, company, platoon, email=email, role=role
+        )
+
+    def update(self: Self, other: Self) -> None:
+        self.first_name, self.last_name = other.first_name, other.last_name
+        self.company = other.company
+        self.active = other.active
+        self.role = other.role
+
+    def __repr__(self: Self) -> str:
+        repr_str = super().__repr__()
+        return f"User({repr_str} email: {self.email}, role: {self.role})"
+      
+
+    def get_id(self: Self) -> str:
+        return self.email
+
+
+@login_manager.user_loader
+def load_user(id: str) -> User:
+    return db.session.get(User, id)
