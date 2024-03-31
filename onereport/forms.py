b5feff4fa4710cee04dd8093f050d5223effbbc5
adler <avihai.adler@gmail.com>
2024-03-31 22:55:59 +0300

    - changed `UserRegistrationFrom` & `UserUpdateForm` to support the new model hierarchy. - removed all dal calls from `forms`. - better feedback on errors. - added more validation functions: `validate_platoon`, `validate_email` & `validate_id` to various forms.

--- onereport/forms.py
+++ onereport/forms.py
@@ -3,10 +3,11 @@ import flask_wtf
 import wtforms
 import flask_login
 import wtforms.validators as validators
+import email_validator
 from onereport.data import misc
-from onereport.dal import personnel_dal, user_dal, order_attr
-from onereport.dto import personnel_dto
+from onereport.dal import order_attr
 
+ID_LEN = 7
 
 class PersonnelRegistrationFrom(flask_wtf.FlaskForm):
     id = wtforms.StringField(
@@ -37,25 +38,29 @@ class PersonnelRegistrationFrom(flask_wtf.FlaskForm):
     submit = wtforms.SubmitField("הוסף חייל")
 
     def validate_id(self: Self, id: wtforms.StringField) -> None:
-        personnel = personnel_dal.find_personnel_by_id(id.data)
-        if personnel:
-            raise wtforms.ValidationError("Personnel already exists")
+        if len(id.data) < ID_LEN:
+            raise wtforms.ValidationError("המס' האישי חייב להכיל לפחות {ID_LEN} תווים")
+        try:
+            int(id.data)
+        except ValueError as _:
+            raise wtforms.ValidationError("מס' אישי לא תקין")
 
     def validate_company(self: Self, company: wtforms.SelectField) -> None:
         if not misc.Company.is_valid(company.data):
-            raise wtforms.ValidationError("Invalid company")
+            raise wtforms.ValidationError("ערך לא תקין")
 
     def validate_platoon(self: Self, platoon: wtforms.SelectField) -> None:
         if not misc.Platoon.is_valid(platoon.data):
-            raise wtforms.ValidationError("Invalid platoon")
+            raise wtforms.ValidationError("ערך לא תקין")
 
 
 class UserRegistrationFrom(flask_wtf.FlaskForm):
+    id = wtforms.StringField("מספר אישי")
     email = wtforms.EmailField(
         "אימייל",
         validators=[
             validators.InputRequired("שדה חובה"),
-            validators.Email("שדה לא תקין"),
+            # validators.Email("שדה לא תקין"),
         ],
     )
     first_name = wtforms.StringField(
@@ -72,6 +77,12 @@ class UserRegistrationFrom(flask_wtf.FlaskForm):
             (name, member.value) for name, member in misc.Company._member_map_.items()
         ],
     )
+    platoon = wtforms.SelectField(
+        "מחלקה",
+        choices=[
+            (name, member.value) for name, member in misc.Platoon._member_map_.items()
+        ],
+    )
     role = wtforms.SelectField(
         "תפקיד",
         choices=[
@@ -81,24 +92,25 @@ class UserRegistrationFrom(flask_wtf.FlaskForm):
 
     submit = wtforms.SubmitField("הוסף משתמש")
 
-    def validate_email(self: Self, email: wtforms.StringField) -> None:
-        user = user_dal.find_user_by_email(email.data)
-        if user:
-            raise wtforms.ValidationError("User already exists")
+    def validate_email(self: Self, email: wtforms.EmailField) -> None:
+        try:
+            email_validator.validate_email(email.data, check_deliverability=False)
+        except email_validator.EmailNotValidError as _:
+            raise wtforms.ValidationError("אימייל לא תקין")
 
     def validate_role(self: Self, role: wtforms.SelectField) -> None:
         if not misc.Role.is_valid(role.data):
-            raise wtforms.ValidationError("Invalid role")
+            raise wtforms.ValidationError("תפקיד לא תקין")
 
         current_user_role = flask_login.current_user.role
         if not misc.Role.is_valid(current_user_role):
-            raise wtforms.ValidationError("Invalid role")
+            raise wtforms.ValidationError("אין לך הרשאה לבצע פעולה זו")
 
         if (
             misc.Role[current_user_role] != misc.Role.ADMIN
             and role.data == misc.Role.ADMIN.name
         ):
-            raise wtforms.ValidationError("Permission denied")
+            raise wtforms.ValidationError("אינך רשאי.ת ליצור משתמש עם הרשאה זו")
 
 
 class PersonnelListForm(flask_wtf.FlaskForm):
@@ -121,11 +133,11 @@ class PersonnelListForm(flask_wtf.FlaskForm):
 
     def validate_order_by(self: Self, order_by: wtforms.SelectField) -> None:
         if not order_attr.PersonnelOrderBy.is_valid(order_by.data):
-            raise wtforms.ValidationError("Invalid order by")
+            raise wtforms.ValidationError("ערך לא תקין")
 
     def validate_order(self: Self, order: wtforms.SelectField) -> None:
         if not order_attr.Order.is_valid(order.data):
-            raise wtforms.ValidationError("Invalid order")
+            raise wtforms.ValidationError("ערך לא תקין")
 
 
 class PersonnelUpdateForm(flask_wtf.FlaskForm):
@@ -161,19 +173,20 @@ class PersonnelUpdateForm(flask_wtf.FlaskForm):
 
     def validate_company(self: Self, company: wtforms.SelectField) -> None:
         if not misc.Company.is_valid(company.data):
-            raise wtforms.ValidationError("Invalid company")
+            raise wtforms.ValidationError("ערך לא תקין")
 
     def validate_active(self: Self, active: wtforms.SelectField) -> None:
         if not misc.Active.is_valid(active.data):
-            raise wtforms.ValidationError("invalid value")
+            raise wtforms.ValidationError("ערך לא תקין")
 
     def validate_platoon(self: Self, platoon: wtforms.SelectField) -> None:
         if not misc.Platoon.is_valid(platoon.data):
-            raise wtforms.ValidationError("Invalid platoon")
+            raise wtforms.ValidationError("ערך לא תקין")
 
 
 class UserUpdateForm(flask_wtf.FlaskForm):
-    email = wtforms.StringField("אימייל")
+    id = wtforms.StringField("מספר אישי")
+    email = wtforms.EmailField("אימייל")
     first_name = wtforms.StringField(
         "שם פרטי",
         validators=[validators.InputRequired("שדה חובה"), validators.Length(2, 10)],
@@ -194,6 +207,12 @@ class UserUpdateForm(flask_wtf.FlaskForm):
             (name, member.value) for name, member in misc.Company._member_map_.items()
         ],
     )
+    platoon = wtforms.SelectField(
+        "מחלקה",
+        choices=[
+            (name, member.value) for name, member in misc.Platoon._member_map_.items()
+        ],
+    )
     active = wtforms.SelectField(
         "פעיל",
         choices=[
@@ -202,38 +221,41 @@ class UserUpdateForm(flask_wtf.FlaskForm):
     )
 
     submit = wtforms.SubmitField("עדכן")
+    delete = wtforms.SubmitField("הסר")
 
     def validate_company(self: Self, company: wtforms.SelectField) -> None:
         if not misc.Company.is_valid(company.data):
-            raise wtforms.ValidationError("Invalid company")
+            raise wtforms.ValidationError("ערך לא תקין")
 
     def validate_active(self: Self, active: wtforms.SelectField) -> None:
         if not misc.Active.is_valid(active.data):
-            raise wtforms.ValidationError("invalid value")
+            raise wtforms.ValidationError("ערך לא תקין")
+
+    def validate_platoon(self: Self, platoon: wtforms.SelectField) -> None:
+        if not misc.Platoon.is_valid(platoon.data):
+            raise wtforms.ValidationError("ערך לא תקין")
+
+    def validate_email(self: Self, email: wtforms.EmailField) -> None:
+        try:
+            email_validator.validate_email(email.data, check_deliverability=False)
+        except email_validator.EmailNotValidError as _:
+            raise wtforms.ValidationError("אימייל לא תקין")
 
     def validate_role(self: Self, role: wtforms.SelectField) -> None:
         if not misc.Role.is_valid(role.data):
-            raise wtforms.ValidationError("Invalid role")
+            raise wtforms.ValidationError("תפקיד לא תקין")
 
         current_user_role = flask_login.current_user.role
         if not misc.Role.is_valid(current_user_role):
-            raise wtforms.ValidationError("Invalid role")
+            raise wtforms.ValidationError("אין לך הרשאה לבצע פעולה זו")
 
         if (
             misc.Role[current_user_role] != misc.Role.ADMIN
             and role.data == misc.Role.ADMIN.name
         ):
-            raise wtforms.ValidationError("Permission denied")
+            raise wtforms.ValidationError("אינך רשאי.ת ליצור משתמש עם הרשאה זו")
 
 
 class UpdateReportForm(flask_wtf.FlaskForm):
     submit = wtforms.SubmitField("שלח")
 
-    def append_all(self: Self, personnel: list[personnel_dto.PersonnelDTO]) -> None:
-        for p in personnel:
-            self.personnel.append_entry(wtforms.BooleanField())
-            self.personnel.entries[-1].data = False
-            self.personnel.entries[-1].id = p.id
-
-        for entry in self.personnel.entries:
-            print(entry)
