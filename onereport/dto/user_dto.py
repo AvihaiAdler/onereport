28d7ebe70380b220837c00c8e11f959ef88d6841
adler <avihai.adler@gmail.com>
2024-03-31 22:52:52 +0300

    added `id` & `platoon` to `UserDTO` attributes'

--- onereport/dto/user_dto.py
+++ onereport/dto/user_dto.py
@@ -5,9 +5,11 @@ from onereport.data import model
 
 class UserDTO:
     def __init__(self: Self, user: model.User) -> None:
+        self.id = user.id
         self.first_name = user.first_name
         self.last_name = user.last_name
         self.email = user.email
         self.role = misc.Role[user.role].value
         self.company = misc.Company[user.company].value
+        self.platoon = misc.Platoon[user.platoon].value
         self.active = "פעיל" if user.active else "לא פעיל"
