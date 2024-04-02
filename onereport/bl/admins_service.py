8372e0f574eb2381491e3090d7b19b1aaad64d3b
adler <avihai.adler@gmail.com>
2024-04-02 20:04:00 +0300

    refactor admins' business logic into its own `admins_service`

---
+++ onereport/bl/admins_service.py
@@ -0,0 +1,66 @@
+from onereport import app
+from onereport.dal import personnel_dal, user_dal
+from onereport.dal.order_attr import Order, PersonnelOrderBy, UserOrderBy
+from onereport.dto.user_dto import UserDTO
+from onereport.dto.personnel_dto import PersonnelDTO
+from onereport.exceptions.exceptions import (
+    BadRequestError,
+    NotFoundError,
+)
+from onereport.forms import PersonnelListForm
+
+
+def get_all_users(order_by: str, order: str, /) -> list[UserDTO]:
+    """
+    Raises:
+        BadRequestError,
+        NotFoundError
+    """
+    if not UserOrderBy.is_valid(order_by):
+        app.logger.error(f"invalid order_by: {order_by}")
+        raise BadRequestError(f"מיון לפי {order_by} אינו נתמך")
+
+    if not Order.is_valid(order):
+        app.logger.error(f"invalid order: {order}")
+        raise BadRequestError(f"סדר {order} אינו נתמך")
+
+    users = user_dal.find_all_users(UserOrderBy[order_by], Order[order])
+    if not users:
+        app.logger.warning("USER table is empty")
+        raise NotFoundError("רשימת המשתמשים הינה ריקה")
+
+    return [UserDTO(user) for user in users]
+
+
+def get_all_personnel(
+    form: PersonnelListForm, order_by: str, order: str, /
+) -> list[PersonnelDTO]:
+    """
+    Raises:
+        BadRequestError,
+        NotFoundError
+    """
+    if form is None:
+        app.logger.error(f"invalid form {form}")
+        raise BadRequestError("form must not be None")
+
+    if not PersonnelOrderBy.is_valid(order_by):
+        app.logger.error(f"invalid order_by: {order_by}")
+        raise BadRequestError(f"מיון לפי {order_by} אינו נתמך")
+
+    if not Order.is_valid(order):
+        app.logger.error(f"invalid order: {order}")
+        raise BadRequestError(f"סדר {order} אינו נתמך")
+
+    if form.validate_on_submit():
+        order_by = form.order_by.data
+        order = form.order.data
+
+    personnel = personnel_dal.find_all_personnel(
+        PersonnelOrderBy[order_by], Order[order]
+    )
+    if not personnel:
+        app.logger.warning("personnel table is empty")
+        raise NotFoundError("רשימת החיילים הינה ריקה")
+
+    return [PersonnelDTO(p) for p in personnel]
--- onereport/endpoints/admins.py
+++ onereport/endpoints/admins.py
@@ -1,9 +1,10 @@
 from onereport import app, forms, generate_urlstr
+from onereport.bl import admins_service
 from onereport.data import misc
-from onereport.dto import user_dto, personnel_dto
-from onereport.dal import user_dal, personnel_dal, order_attr
 from flask import url_for, redirect, flash, render_template, request
 from flask_login import current_user, login_required
+
+from onereport.exceptions.exceptions import BadRequestError, NotFoundError
 
 
 def not_permitted() -> bool:
@@ -73,33 +74,16 @@ def a_get_all_users() -> str:
 
     order_by = request.args.get("order_by", default="COMPANY")
     order = request.args.get("order", "ASC")
-    app.logger.debug(
-        f"query all users for {current_user}\nquery params: order by: {order_by}, order: {order}"
-    )
 
-    if not order_attr.UserOrderBy.is_valid(order_by):
-        app.logger.warning(f"received incorrect query param order by: {order_by}")
-        flash(f"אין אפשרות לסדר את העצמים לפי {order_by}", category="info")
+    try:
+        users = admins_service.get_all_users(order_by, order)
+        return render_template("users/users.html", users=users)
+    except BadRequestError as be:
+        flash(str(be), category="danger")
+    except NotFoundError as ne:
+        flash(str(ne), category="info")
 
-        return render_template("users/users.html", users=[])
-
-    if not order_attr.Order.is_valid(order):
-        app.logger.warning(f"received incorrect query param order: {order}")
-        flash(f"אין אפשרות לסדר את העצמים בסדר {order}", category="info")
-
-        return render_template("users.html", users=[])
-
-    users = user_dal.find_all_users(
-        order_attr.UserOrderBy[order_by], order_attr.Order[order]
-    )
-
-    if not users:
-        app.logger.warning("users table is empty")
-
-    app.logger.debug(f"passing {len(users)} users to users.html for {current_user}")
-    return render_template(
-        "users/users.html", users=[user_dto.UserDTO(user) for user in users]
-    )
+    return redirect(url_for("home"))
 
 
 @app.route("/onereport/admins/personnel", methods=["GET", "POST"])
@@ -111,42 +95,21 @@ def a_get_all_personnel() -> str:
 
     order_by = request.args.get("order_by", default="LAST_NAME")
     order = request.args.get("order", "ASC")
-    app.logger.debug(
-        f"query all users for {current_user}\nquery params: order by: {order_by}, order: {order}"
-    )
 
     form = forms.PersonnelListForm()
-    if not order_attr.PersonnelOrderBy.is_valid(order_by):
-        app.logger.warning(f"received incorrect query param order by: {order_by}")
-        flash(f"אין אפשרות לסדר את העצמים לפי {order_by}", category="info")
+    try:
+        personnel = admins_service.get_all_personnel(form, order_by, order)
+        return render_template(
+            "personnel/personnel_list.html",
+            form=form,
+            personnel=personnel,
+        )
+    except BadRequestError as be:
+        flash(str(be), category="danger")
+    except NotFoundError as ne:
+        flash(str(ne), category="info")
 
-        return render_template("personnel/personnel_list.html", form=form, personnel=[])
-
-    if not order_attr.Order.is_valid(order):
-        app.logger.warning(f"received incorrect query param order: {order}")
-        flash(f"אין אפשרות לסדר את העצמים בסדר {order}", category="info")
-
-        return render_template("personnel/personnel_list.html", form=form, personnel=[])
-
-    if form.validate_on_submit():
-        order_by = order_attr.PersonnelOrderBy[form.order_by.data]
-        order = order_attr.Order[form.order.data]
-    elif request.method == "GET":
-        order_by = order_attr.PersonnelOrderBy[order_by]
-        order = order_attr.Order[order]
-
-    personnel = personnel_dal.find_all_personnel(order_by, order)
-    if not personnel:
-        app.logger.warning("personnel table is empty")
-
-    app.logger.debug(
-        f"passing {len(personnel)} personnel to personnel_list.html for {current_user}"
-    )
-    return render_template(
-        "personnel/personnel_list.html",
-        form=form,
-        personnel=[personnel_dto.PersonnelDTO(p) for p in personnel],
-    )
+    return redirect(url_for("home"))
 
 
 @app.route("/onereport/admins/report", methods=["GET", "POST"])
@@ -168,9 +131,6 @@ def a_get_all_reports() -> str:
 
     company = request.args.get("company", default="")
     order = request.args.get("order", default="DESC")
-    app.logger.debug(
-        f"query all reports for {current_user}\nquery params: company: {company}, order: {order}"
-    )
 
     return redirect(
         url_for(
