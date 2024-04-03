2d1ceb64d1ac4a2e930602874a6c130d42e2f5f3
adler <avihai.adler@gmail.com>
2024-04-03 16:30:32 +0300

    added `from_user` to convert a `User` into a `PersonnelDTO`

--- onereport/dto/personnel_dto.py
+++ onereport/dto/personnel_dto.py
@@ -11,6 +11,19 @@ class PersonnelDTO:
         self.company = misc.Company[personnel.company].value
         self.platoon = misc.Platoon[personnel.platoon].value
         self.active = personnel.active
+        
+    @staticmethod
+    def from_user(user: model.User) -> Self:
+        personnel = model.Personnel(
+            user.id,
+            user.first_name,
+            user.last_name,
+            user.company,
+            user.platoon,
+        )
+        personnel.active = user.active
+        return PersonnelDTO(personnel)
+        
 
     def __repr__(self: Self) -> str:
         return f"PersonnelDTO(id: {self.id}, first name: {self.first_name}, last name: {self.first_name}, company: {self.company}, platoon: {self.platoon}, active: {self.active})"
