0667fef2676b39b0f143322e38d5ea8079d1325e
adler <avihai.adler@gmail.com>
2024-04-07 13:52:21 +0300

    - fixed bad `redirect` calls - aded functionality to delete all personnel, delete all reports & upload personnel via json file

--- onereport/endpoints/admins.py
+++ onereport/endpoints/admins.py
@@ -3,8 +3,7 @@ from onereport.bl import admins_service
 from onereport.data import misc
 from flask import url_for, redirect, flash, render_template, request
 from flask_login import current_user, login_required
-
-from onereport.exceptions.exceptions import BadRequestError, NotFoundError
+from onereport.exceptions.exceptions import BadRequestError, InternalServerError, NotFoundError
 
 
 def not_permitted() -> bool:
@@ -20,7 +19,7 @@ def not_permitted() -> bool:
 def a_register_personnel() -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
+        return redirect(url_for("home"))
 
     return redirect(
         url_for(generate_urlstr(misc.Role.MANAGER.name, "register_personnel"))
@@ -32,7 +31,7 @@ def a_register_personnel() -> str:
 def a_register_user(id: str) -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
+        return redirect(url_for("home"))
 
     return redirect(
         url_for(generate_urlstr(misc.Role.MANAGER.name, "register_user"), id=id)
@@ -44,7 +43,7 @@ def a_register_user(id: str) -> str:
 def a_update_personnel(id: str) -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
+        return redirect(url_for("home"))
 
     return redirect(
         url_for(generate_urlstr(misc.Role.MANAGER.name, "update_personnel"), id=id)
@@ -56,7 +55,7 @@ def a_update_personnel(id: str) -> str:
 def a_update_user(email: str) -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
+        return redirect(url_for("home"))
 
     return redirect(
         url_for(generate_urlstr(misc.Role.MANAGER.name, "update_user"), email=email)
@@ -120,8 +119,7 @@ def a_get_all_personnel() -> str:
 def a_create_report() -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
-
+        return redirect(url_for("home"))
     return redirect(url_for(generate_urlstr(misc.Role.MANAGER.name, "create_report")))
 
 
@@ -130,7 +128,7 @@ def a_create_report() -> str:
 def a_get_all_reports() -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
+        return redirect(url_for("home"))
 
     company = request.args.get("company", default=current_user.company)
     order = request.args.get("order", default="DESC")
@@ -149,10 +147,59 @@ def a_get_all_reports() -> str:
 def a_get_report(id: int) -> str:
     if not_permitted():
         app.logger.warning(f"unauthorized access by {current_user}")
-        return redirect("home")
+        return redirect(url_for("home"))
 
     company = request.args.get("company", current_user.company)
     
     return redirect(
         url_for(generate_urlstr(misc.Role.MANAGER.name, "get_report"), id=id, company=company)
     )
+    
+    
+@app.route("/onereport/admins/personnel/upload", methods=["GET", "POST"])
+@login_required
+def upload_personnel() -> str:
+    if not_permitted():
+        app.logger.warning(f"unauthorized access by {current_user}")
+        return redirect(url_for("home"))
+    
+    form = forms.UploadPersonnelForm()
+    try:
+        admins_service.upload_personnel(form)
+        
+        return render_template("personnel/upload_personnel.html", form=form)
+    except BadRequestError as be:
+        flash(f"{be}", category="danger")
+    except InternalServerError as ie:
+        flash(f"{ie}", category="danger")
+        
+    return redirect(url_for("home"))
+
+
+@app.get("/onereport/admins/report/delete")
+@login_required
+def delete_all_reports() -> str:
+    if not_permitted():
+        app.logger.warning(f"unauthorized access by {current_user}")
+        return redirect(url_for("home"))
+    
+
+    if admins_service.delete_all_reports():
+        flash("כל הדוחות נמחקו בהצלחה", category="success")
+    else:
+        flash("שגיאת שרת", category="danger")
+    return redirect(url_for("home"))
+
+@app.get("/onereport/admins/personnel/delete")
+@login_required
+def delete_all_personnel() -> str:
+    if not_permitted():
+        app.logger.warning(f"unauthorized access by {current_user}")
+        return redirect(url_for("home"))
+    
+
+    if admins_service.delete_all_personnel():
+        flash("כל הדוחות נמחקו בהצלחה", category="success")
+    else:
+        flash("שגיאת שרת", category="danger")
+    return redirect(url_for("home"))
