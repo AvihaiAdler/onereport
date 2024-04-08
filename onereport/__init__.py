b09842c8fc0f4ad7ae71830723bb4cb62977550b
adler <avihai.adler@gmail.com>
2024-04-08 16:18:07 +0300

    - added `get_company_value_by_name`. - implemented a better loding mechanism for `SQLALCHEMY_DATABASE_URI`

--- onereport/__init__.py
+++ onereport/__init__.py
@@ -12,7 +12,7 @@ import os
 # Redirect URIs must use the HTTPS scheme, not plain HTTP
 
 # since its a relative path - a better solution should be in place
-if not dotenv.load_dotenv(dotenv_path="resources/.env"):   
+if not dotenv.load_dotenv(dotenv_path="resources/.env"):
     logging.error("failed to load environment variables")
     exit(1)
 
@@ -25,12 +25,10 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", None)
 if app.config["SECRET_KEY"] is None:
     app.logger.error("failed to load in secret_key")
     exit(1)
-    
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI", None)
-if app.config["SQLALCHEMY_DATABASE_URI"] is None:
-    app.logger.error("failed to load in db uri")
-    exit(1)
-    
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI", "sqlite:///site.db"
+)
 app.config["OAUTH2_PROVIDERS"] = {
     # Google OAuth 2.0 documentation:
     # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
@@ -49,6 +47,7 @@ app.config["OAUTH2_PROVIDERS"] = {
 
 app.config["PHONE"] = os.environ.get("PHONE", None)
 app.config["EMAIL"] = os.environ.get("EMAIL", None)
+
 
 @app.template_filter()
 def generate_urlstr(role_name: str, urlstr: str) -> str:
@@ -77,12 +76,24 @@ def checked(present: bool) -> str:
 def is_active(active: bool) -> str:
     return "checked" if active else ""
 
+
 @app.template_filter()
 def get_company_name_by_value(company_value: str) -> str:
     return misc.Company(company_value).name
 
+
+@app.template_filter()
+def get_company_value_by_name(company_name: str) -> str:
+    return (
+        misc.Company[company_name].value
+        if misc.Company.is_valid(company_name)
+        else "לא תקינה"
+    )
+
+
 model.db.init_app(app=app)
 model.login_manager.init_app(app=app)
+
 
 def drop_all() -> None:
     with app.app_context():
@@ -110,6 +121,7 @@ def register_personnel(personnel: model.Personnel) -> None:
     with app.app_context():
         model.db.session.add(personnel)
         model.db.session.commit()
+
 
 from onereport.endpoints import common      # noqa: E402, F401
 from onereport.endpoints import users       # noqa: E402, F401
