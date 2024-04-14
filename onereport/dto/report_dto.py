730432a0e10de573134f96f8eba7238cbcf4e743
adler <avihai.adler@gmail.com>
2024-04-14 12:00:11 +0300

    minor changes

--- onereport/dto/report_dto.py
+++ onereport/dto/report_dto.py
@@ -8,7 +8,7 @@ class ReportDTO():
     self.id = report.id
     self.date = report.date
     self.company = misc.Company[report.company].value
-    self.presence = [(PersonnelDTO(p), p in report.presence) for p in personnel]
+    self.presence = [(PersonnelDTO(p), p in report.presence) for p in personnel] 
     
   def __repr__(self: Self) -> str:
     return f"Report(id: {self.id}, date: {self.date}, company: {self.company})"
